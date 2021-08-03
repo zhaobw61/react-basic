79bb4966b82517689531cfc2ffbf12ea88a296c9
zhaobowen <bowen.zhao@chiefclouds.com>
2021-08-03 18:59:10 +0800

    初始化Hook

--- hook/src/index.js
+++ hook/src/index.js
@@ -1,2 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+class Child extends React.Component {
+    render() {
+        return (
+            <>
+                <span>你好 Hook</span>
+            </>
+        )
+    }
+}
+
+ReactDOM.render(<Child/>, document.getElementById('root'));
