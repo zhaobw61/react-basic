069b48f8df9ec832717188e2f06141cada8bbfc5
zhaobowen <bowen.zhao@chiefclouds.com>
2021-08-20 18:53:58 +0800

    init router

--- router/src/index.js
+++ router/src/index.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
+import {BrowserRouter as Router, Route} from 'react-router-dom';
+import Home from './components/Home';
+import Profile from './components/Profile';
+import User from './components/User';
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <>
+        <Route path="/" component={Home}/>
+        <Route path="/user" component={User}/>
+        <Route path="/profile" component={Profile}/>
+      </>
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
