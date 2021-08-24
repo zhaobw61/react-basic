6fe8abaee4e0c8c7d8493be9d0bb3f0bf247ab0e
zhaobowen <bowen.zhao@chiefclouds.com>
2021-08-24 11:45:57 +0800

    修改为class组件

---
+++ redux/src/components/Counter.js
@@ -0,0 +1,36 @@
+import {createStore} from '../redux';
+import React from "react";
+const ADD = 'ADD';
+const MINUS = 'MINUS';
+
+function reducer(state={number: 0}, action) {
+  switch (action.type) {
+    case ADD:
+      return {number:state.number + 1};
+    case MINUS:
+      return {number:state.number - 1};
+    default:
+      return state;
+  }
+}
+let store = createStore(reducer);
+export default class Counter extends React.Component{
+  state = {number: store.getState().number};
+  componentDidMount() {
+    store.subscribe(()=>{
+      this.unSubscribe = this.setState({number: store.getState().number});
+    })
+  }
+  componentWillUnmount() {
+    this.unSubscribe && this.unSubscribe();
+  }
+  render(h) {
+    return(
+      <div>
+        <p>{this.state.number}</p>
+        <button onClick={() => store.dispatch({type:ADD})}>+</button>
+        <button onClick={() => store.dispatch({type:MINUS})}>-</button>
+      </div>
+    )
+  }
+}
--- redux/src/index.js
+++ redux/src/index.js
@@ -1,46 +1,5 @@
-import createStore from './redux/createStore'
-let counter = document.getElementById('counter');
-let addBtn = document.getElementById('add');
-let minusBtn = document.getElementById('minus');
-const ADD = 'ADD';
-const MINUS = 'MINUS';
+import React from "react";
+import ReactDOM from 'react-dom'
+import Counter from "./components/Counter";
 
-function reducer(state={number: 0}, action) {
-  switch (action.type) {
-    case ADD:
-      return {number:state.number + 1};
-    case MINUS:
-      return {number:state.number - 1};
-    default:
-      return state;
-  }
-}
-let store = createStore(reducer);
-addBtn.addEventListener('click',()=>{
-  store.dispatch({type: ADD});
-})
-minusBtn.addEventListener('click',()=>{
-  store.dispatch({type: MINUS});
-})
-store.subscribe(render);
-function render() {
-  counter.innerText = store.getState().number
-}
-render();
-/**
- * 如果传入新的对象的，咋办？ 貌似只能通过action 来修改
- * reducer为什么是一个函数
- */
-
-/**
- * 语法：
- * createStore
- * dispatch
- * subscribe
- * getState
- * 没有办法监听一个字段
- */
-
-
-
-
+ReactDOM.render(<Counter/>, document.getElementById('root'));
